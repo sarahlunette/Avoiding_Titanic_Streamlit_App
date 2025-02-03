08ca4371caab1dd2212a1d7b8aa701a7564344f3
Sarah Le Net <sarahlunette@users.noreply.github.com>
2025-02-03 17:35:50 +0100

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -11,6 +11,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
+import sys
+
+sys.path.append('/images')
 
 pages = ['Statistics on Icebreaker', 'Prediction of icebergs', 'Is Leonardo DiCaprio really dead ?']
 
